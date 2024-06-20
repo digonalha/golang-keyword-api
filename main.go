f81d3244e5ad0344aaa608170b9dad29f33061dd
Rodrigo Fernandes <digonalha@gmail.com>
2024-06-20 20:58:18 -0300

    feat: test gin context reader

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"keyword-api/src/handlers"
+	keyword_handler "keyword-api/src/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func createRouter() {
 	router := gin.Default()
-	router.POST("/keyword", handlers.CreateKeyword)
+
+	userRoutes := router.Group("/users")
+	{
+		userRoutes.GET("/:userId/keywords", keyword_handler.GetKeywordsByUserId)
+	}
+
+	keywordRoutes := router.Group("/keywords")
+	{
+		keywordRoutes.POST("", keyword_handler.CreateKeyword)
+		keywordRoutes.PUT("/:id", keyword_handler.UpdateKeyword)
+		keywordRoutes.DELETE("/:id", keyword_handler.DeleteKeyword)
+	}
 
 	router.Run(":8001")
+}
+
+func main() {
+	createRouter()
 }
--- src/handlers/keyword_handler.go
+++ src/handlers/keyword_handler.go
@@ -1,11 +1,27 @@
-package handlers
+package keyword_handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateKeyword(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "keyword created"})
+	c.JSON(http.StatusCreated, gin.H{"message": "keyword created"})
+}
+
+func GetKeywordsByUserId(c *gin.Context) {
+	userId := c.Param("userId")
+	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("List keywords from user %s", userId)})
+}
+
+func UpdateKeyword(c *gin.Context) {
+	keywordId := c.Param("id")
+	c.JSON(http.StatusAccepted, gin.H{"message": fmt.Sprintf("keyword %s updated", keywordId)})
+}
+
+func DeleteKeyword(c *gin.Context) {
+	keywordId := c.Param("id")
+	c.JSON(http.StatusAccepted, gin.H{"message": fmt.Sprintf("keyword %s deleted", keywordId)})
 }
